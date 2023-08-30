69a7efedfe02ef988963bb7b35576b5c24c280de
KETANPATI <119190502+KETANPATI@users.noreply.github.com>
2023-08-31 01:39:27 +0530

    Add files via upload

---
+++ music_list.js
@@ -0,0 +1,135 @@
+let allMusic = [
+    {
+        name: "Ekadantaya Vakratundaya Gauri Tanaya Dhimi",
+        artist: "Shankar Mahadevan",
+        img: "music-0",
+        src: "music-0"
+    },
+    {
+        name: "Yugat Mandli",
+        artist: "Digpal Lanjekar",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Calm-Down",
+        artist: "Rema, Selena Gomez",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Let Me Down Slowly",
+        artist: "Alec Benjamin",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Love Me Like You Do",
+        artist: "Ellie Goulding",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Starboy",
+        artist: "The Weeknd",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Girls Like You",
+        artist: "Maroon 5",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "On My Way",
+        artist: "Alan Walker",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Sorry",
+        artist: "Justin Beiber",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Sugar and Brownies",
+        artist: "Dharia",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Allah Maaf Kare",
+        artist: "DESI BOYZ",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Jeena Jeena ",
+        artist: "Atif Aslam ",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Aapka Kya Hoga",
+        artist: "Mika Singh, Sajid Khan, and Sunidhi Chauhan",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Piya o re piya ",
+        artist: "Atif aslam ",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "A2-Tum Hi Ho",
+        artist: "Aditya Roy Kapur, Shraddha Kapoor",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Aaj Dil Shayarana- Holiday",
+        artist: "Irshad Kamil",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Nagada Sang Dhol ",
+        artist: "Shreya Ghoshal",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Shape-of-You",
+        artist: "Ed Sheeran",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "Udd Jaa Kaale Kaava ",
+        artist: "Uttam S,Anand Bakshi",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "Kali Kali Zulfon Ke",
+        artist: "Ustad Nusrat Fateh Ali Khan",
+        img: "music-20",
+        src: "music-20"
+    },
+    {
+        name: "jai mahakal",
+        artist: "Amit Trivedi, Swanand K",
+        img: "music-21",
+        src: "music-21"
+    },
+
+];
